914ba39fa6b67ef32a4dbd93bd830af50de1cfbf
york <york@yorkdeMacBook-Pro.local>
2016-04-03 23:17:49 +0800

    Add flux version practice

---
+++ js/components/Main.js
@@ -0,0 +1,55 @@
+import API from '../API';
+import React, {Component} from 'react';
+import LinkStore from '../stores/LinkStore';
+
+const _getAppStore = () => {
+    return {
+        links: LinkStore.getAll()
+    };
+}
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = _getAppStore();
+        this.onChange = this.onChange.bind(this);
+    }
+
+    componentWillUnmount() {
+        LinkStore.removeListener('change', this.onChange);
+    }
+
+
+    onChange() {
+        console.log('4. In onChange');
+        this.setState(_getAppStore());
+    }
+
+    componentDidMount() {
+        API.fetchLinks();
+        LinkStore.on('change', this.onChange);
+    }
+
+
+    render() {
+        const content = this.state.links.map((link) => {
+            return (
+                <li key={link.id}>
+                    <a href={link.url}>{link.title}</a>
+                </li>
+            );
+        });
+
+        return (
+            <div>
+                <h3>Links</h3>
+                <ul>
+                    {content}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Main;
