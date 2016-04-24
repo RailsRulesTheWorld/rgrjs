fe7d5f907480a2876f107dd940318327170ed1d6
RailsRulesTheWorld <wk19930618@gmail.com>
2016-04-24 11:21:41 +0800

    Use async await syntax to replace traditional database access operation code

--- server.js
+++ server.js
@@ -8,23 +8,34 @@ const app = express();
 
 app.use(express.static('public'));
 
-let db;
-console.log(process.env.MONGO_URL);
-MongoClient.connect(process.env.MONGO_URL, (err, database) => {
-  if (err) throw err;
+(async () => {
+  const db = await MongoClient.connect(process.env.MONGO_URL);
 
-  database.collection('links').find({}).toArray((e) => {
-    if (e) throw e;
+  app.use('/graphql', GraphQLHTTP({
+    schema: schema(db),
+    graphiql: true,
+  }));
 
-    db = database;
+  app.listen(3000, () => console.log('Listening on port 3000'));
+})(); // 使用async await语法可以避免无关业务的数据库连接错误等检查代码，更加简洁
 
-    app.use('/graphql', GraphQLHTTP({
-      schema: schema(db),
-      graphiql: true,
-    }));
-    app.listen(3000, () => console.log('Listening on port 3000'));
-  });
-});
+// let db;
+// console.log(process.env.MONGO_URL);
+// MongoClient.connect(process.env.MONGO_URL, (err, database) => {
+//   if (err) throw err;
+
+//   database.collection('links').find({}).toArray((e) => {
+//     if (e) throw e;
+
+//     db = database;
+
+//     app.use('/graphql', GraphQLHTTP({
+//       schema: schema(db),
+//       graphiql: true,
+//     }));
+//     app.listen(3000, () => console.log('Listening on port 3000'));
+//   });
+// });
 
 // app.get('/data/links', (req, res) => {
 //     db.collection('links').find({}).toArray((err, links) => {
